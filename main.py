dd6431dbf483888cad1c3c32852088394d7469ba
Firge <59466895+Firge@users.noreply.github.com>
2022-04-04 12:45:41 +1000

    Exceptions added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from data import db_session
 from data.__all_models import User, Comment, Article
 from data import schemas
@@ -11,6 +11,8 @@ db_session.global_init('db/blogs.db')
 async def get_user(user_id: int):
     session = db_session.create_session()
     user = session.query(User).get(user_id)
+    if not user:
+        raise HTTPException(404, 'User not found')
     return user
 
 
@@ -18,6 +20,8 @@ async def get_user(user_id: int):
 async def get_user_comments(author_id: int):
     session = db_session.create_session()
     comments = session.query(Comment).filter_by(author=author_id).all()
+    if not comments:
+        raise HTTPException(404, "User's comments not found")
     return comments
 
 
@@ -25,6 +29,8 @@ async def get_user_comments(author_id: int):
 async def get_user_articles(author_id: int):
     session = db_session.create_session()
     articles = session.query(Article).filter_by(author=author_id).all()
+    if not articles:
+        raise HTTPException(404, "User's articles not found")
     return articles
 
 
@@ -32,6 +38,8 @@ async def get_user_articles(author_id: int):
 async def get_article(article_id: int):
     session = db_session.create_session()
     article = session.query(Article).get(article_id)
+    if not article:
+        raise HTTPException(404, 'Article not found')
     return article
 
 
@@ -39,49 +47,68 @@ async def get_article(article_id: int):
 async def get_comment(comment_id: int):
     session = db_session.create_session()
     article = session.query(Comment).get(comment_id)
+    if not article:
+        raise HTTPException(404, 'Comment not found')
     return article
 
 
 @app.delete('/api/delete_user/{user_id}')
 async def delete_user(user_id: int):
     session = db_session.create_session()
-    session.query(User).filter_by(id=user_id).delete()
+    user = session.query(User).filter_by(id=user_id)
+    if not user:
+        raise HTTPException(400, 'User not exists')
+    user.delete()
+    session.commit()
     return 'Ok'
 
 
 @app.delete('/api/delete_comment/{comment_id}')
 async def delete_comment(comment_id: int):
     session = db_session.create_session()
-    session.query(Comment).filter_by(id=comment_id).delete()
+    comment = session.query(Comment).filter_by(id=comment_id)
+    if not comment:
+        raise HTTPException(400, 'Comment not exists')
+    comment.delete()
+    session.commit()
     return 'Ok'
 
 
 @app.delete('/api/delete_article/{article_id}')
 async def delete_article(article_id: int):
     session = db_session.create_session()
-    session.query(Article).filter_by(id=article_id).delete()
+    article = session.query(Article).filter_by(id=article_id)
+    if not article:
+        raise HTTPException(400, 'Article not exists')
+    article.delete()
+    session.commit()
     return 'Ok'
 
 
 @app.post('/api/change_comment/{comment_id}', response_model=schemas.Comment)
 async def change_comment(comment_id: int, new_comment: schemas.Comment):
     session = db_session.create_session()
-    session.query(Comment).get(comment_id).update(new_comment)
+    comment = session.query(Comment).get(comment_id)
+    if not comment:
+        raise HTTPException(400, 'Comment not exists')
+    comment.update(new_comment)
     session.commit()
     return session.query(Comment).get(comment_id)
 
 
 @app.post('/api/change_user/{user_id}', response_model=schemas.User)
-async def change_comment(user_id: int, new_user: schemas.User):
+async def change_user(user_id: int, new_user: schemas.User):
     session = db_session.create_session()
-    session.query(User).get(user_id).update(new_user)
+    user = session.query(User).get(user_id)
+    if not user:
+        raise HTTPException(400, 'User not exists')
+    user.update(new_user)
     session.commit()
     return session.query(User).get(user_id)
 
 
 @app.post('/api/reg_user', response_model=schemas.User)
 async def reg_user(new_user: schemas.User):
-    print(new_user)
     new_user = User(**new_user.dict())
     session = db_session.create_session()
     session.add(new_user)
